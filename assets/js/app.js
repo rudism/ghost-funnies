3cb259ad97072a4384fce7eefa1be06083727086
rudism <rudism@rudism.xen.prgmr.com>
2014-03-23 01:11:41 +0000

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,88 @@
+var history = window.History;
+var curState = -1;
+history.Adapter.bind(window, 'statechange', function(){
+    var state = history.getState();
+    if(typeof state.data.state === 'undefined' || curState != state.data.state){
+        for(var i = 0; i < postIndex.length; i++){
+            if(state.url.endsWith(postIndex[i].url)){
+                curState = i;
+                break;
+            }
+        }
+    }
+    loadState(state.url);
+});
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+$(document).ready(function(){
+    if(curState < 0 || curState >= postIndex.length){
+        curState = 0;
+        loadState(postIndex[curState].url);
+    }
+});
+function loadState(url){
+    NProgress.start();
+    $.get(url, function(result){
+        var $html = $(result);
+        $('.comic').html($html.find('.content .comic').remove().html());
+        $('.post-header').html($html.find('header').html());
+        $('.post-content').html($html.find('.content').html());
+        switch(curState){
+            case 0:
+                $('.comic-nav').addClass('last-page').removeClass('first-page');
+                break;
+            case postIndex.length - 1:
+                $('.comic-nav').addClass('first-page').removeClass('last-page');
+                break;
+            default:
+                $('.comic-nav').removeClass('last-page').removeClass('first-page');
+                break;
+        }
+        NProgress.done();
+    });
+}
+$('.comic-nav a').click(function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var navTo = $this.parent('li').attr('class');
+    var oldState = curState;
+    switch(navTo){
+        case 'last':
+            if(curState > 0){
+                curState = 0;
+            }
+            break;
+        case 'next':
+            if(curState > 0){
+                curState--;
+            }
+            break;
+        case 'rand':
+            if(postIndex.length > 1){
+                var newState = curState;
+                while(newState == curState){
+                    newState = Math.floor(Math.random() * postIndex.length);
+                }
+                curState = newState;
+            }
+            break;
+        case 'prev':
+            if(curState < postIndex.length - 1){
+                curState++;
+            }
+            break;
+        case 'first':
+            if(curState < postIndex.length - 1){
+                curState = postIndex.length - 1;
+            }
+            break;
+    }
+    if(curState != oldState){
+        history.pushState(
+            {state: curState},
+            postIndex[curState].title,
+            postIndex[curState].url
+        );
+    }
+});
